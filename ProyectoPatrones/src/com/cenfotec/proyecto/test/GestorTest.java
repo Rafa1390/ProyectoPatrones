7c9ceba93048a19ba22566f2b5676c0b38032ac1
Rafael Briceño <rbricenoa@ucenfotec.ac.cr>
2018-06-29 14:59:45 -0600

    Test para comprobación de la actualización de una tarea

--- ProyectoPatrones/src/com/cenfotec/proyecto/test/GestorTest.java
+++ ProyectoPatrones/src/com/cenfotec/proyecto/test/GestorTest.java
@@ -24,5 +24,18 @@ public class GestorTest {
 		
 		assertTrue(tareaTest.getIndicaciones().size() == 1);
 	}
+	
+	@Test
+	public void actualizarTareaTest() throws IOException {
+		Tarea tareaTest = new Tarea();
+		Tarea tareaTestR = new Tarea();
+		ArrayList<String> indicaciones = new ArrayList<String>();
+		ArrayList<String> respuestas = new ArrayList<String>();
+		indicaciones.addAll(Arrays.asList("¿Es mayor de edad?"));
+		respuestas.addAll(Arrays.asList("Si"));
+		tareaTestR = gestor.actualizarTarea(tareaTest, respuestas);
+		
+		assertEquals("Si", tareaTestR.getRespuestas().get(0));
+	}
 
 }
